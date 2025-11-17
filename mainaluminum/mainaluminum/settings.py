34b8ccdc3bc22bad0036092ea5f6a6f5d06ba4cf
Nalimanasa <nalimanasa2004@gmail.com>
2025-11-17 14:14:41 +0000

    debug

--- mainaluminum/mainaluminum/settings.py
+++ mainaluminum/mainaluminum/settings.py
@@ -27,7 +27,7 @@ DEBUG= False
 SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gradtwin-backend.onrender.com', 'localhost', '127.0.0.1', 'gradtwin-frontend-agjg.onrender.com']
 
